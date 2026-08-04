4dcc0a0a84517c4a0a41a1c1af292d4713374035
Yuki Yagi <nsgkr00@gmail.com>
2026-08-04 22:32:32 +0900

    feat: モデル作成とリレーション定義 #34

---
+++ app/Models/ReadingPlan.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\ReadingPlanStatus;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ReadingPlan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'target_date',
+        'completed_at',
+        'status'
+    ];
+
+    /** カラムの型キャスト定義 */
+    protected $casts = [
+        'status' => ReadingPlanStatus::class,
+    ];
+
+    /** モデルのデフォルト値 */
+    protected $attributes = [
+        'status' => 'in_progress',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function scopeOfStatus(Builder $query, ?string $status): Builder
+    {
+        if (empty($status)) {
+            return $query; // 選択されていなければ、絞り込みをせずにそのまま返す
+        }
+        return $query->where('status', $status); // 選択されていれば、whereで絞り込む
+    }
+}
